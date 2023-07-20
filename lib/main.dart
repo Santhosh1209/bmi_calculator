d5a80c92959aed14a8babc21079885966a47bbb4
Santhosh <santhoshgold12@gmail.com>
2023-07-20 21:49:05 +0530

    added gender selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() {
   runApp( bmi()
   );
@@ -42,11 +43,13 @@ class _pageState extends State<page> {
                 Expanded(
                   child: ReusableCard(
                     colour: Color(0xF1D1E33),
-                  )
+                    cardChild: GenderSelect()
+                ),
                 ),
                 Expanded(child:
                 ReusableCard(
                   colour: Color(0xF1D1E33),
+                    cardChild: GenderSelect()
                 )
                 ),
         ],
@@ -78,19 +81,48 @@ class _pageState extends State<page> {
     );
   }
 }
+
+class GenderSelect extends StatelessWidget {
+  const GenderSelect({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+        FontAwesomeIcons.mars,
+          size: 80.0,
+      ),
+        SizedBox(
+          height: 15.0,
+          width: 15.0,
+        ),
+        Text('MALE',
+        style: TextStyle
+          (
+          fontSize: 20.0,
+        ),)
+    ],
+                  );
+  }
+}
 // custom widget created for each container
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour});
+  ReusableCard({required this.colour,this.cardChild});
   // required is used to ensure that colour must always be given as a parameter when calling an object of this class
   // by doing this, we can customize the colour of every unique ResuableCard widget
 
-  // Below, 'final' is used
   final Color colour; // Color property of this class
-  // This is to ensure that the value of colour can only be initialized when calling an object and not otherwise
+  // Here, 'final' is used to ensure that the value of colour can only be initialized when calling an object and not otherwise
+  final Widget? cardChild; // child property of this class ; ? is used to allow null values
   @override
   Widget build(BuildContext context) {
     return Container
       (
+      child: cardChild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
             color: Color(0xFF1D1E33),
